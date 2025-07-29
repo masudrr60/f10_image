ff2ae6ee44a39f69467a3d1a0f7915e224ac4b9b
masudrr60 <masudrr60@gmail.com>
2025-07-30 00:00:59 +0600

    snackbar---alertsdialog--bottomSheet

--- lib/Home.dart
+++ lib/Home.dart
@@ -80,6 +80,7 @@ class _HomeState extends State<Home> {
               );
             });
           },
+
               child: Text(
                 "Alert Dialog",
                 style: TextStyle(fontSize: 25),
