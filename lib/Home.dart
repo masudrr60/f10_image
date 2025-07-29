eae8f7f0df24d49676c8c3519eaad6089a0d73c3
masudrr60 <masudrr60@gmail.com>
2025-07-29 23:59:38 +0600

    sizer and device preview

--- lib/Home.dart
+++ lib/Home.dart
@@ -18,29 +18,74 @@ class _HomeState extends State<Home> {
         title: Text("Image"),
         backgroundColor: Colors.red,
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            // Image.network(
-            //   'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Forget-me-not_Flower.jpg/640px-Forget-me-not_Flower.jpg',
-            // ),
-            Center(
-              child: Container(
-                  height: 50.h,
-                  width: 50.w,
-                  child: Image.asset('asset/image1.webp')),
-            ),
-            Container(
-              height: 30.h,
-              width: 30.w,
-              color: Colors.red,
-            ),
-            Text("Rahat",style: TextStyle(
-              fontSize: 20.sp,
-              color: Colors.blueGrey,
-            ),),
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(onPressed: (){
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text("This is a SnackBar"),
+                duration: Duration(seconds: 2),
+                action: SnackBarAction(
+                  label: "Close",
+                  onPressed: () {
+                  },
+                ),
+              ),
+            );
+          },
+              child: Text("Sanckbar",
+                style: TextStyle(fontSize: 25),
+              )),
+          SizedBox(height: 2.h,),
+          ElevatedButton(onPressed: (){
+           showModalBottomSheet(context: context, builder: (context) {
+             return Column(
+               children: [
+                 Center(
+                   child: Text(
+                     "This is a Bottom Sheet",
+                     style: TextStyle(fontSize: 20.sp),
+                   )),
+                 Container(
+                   height: 100,
+                   width: 50.w,
+                   color: Colors.red,
+                 )
+               ],
+             );
+           });
+          },
+              child: Text(
+                "Bottom Sheet",
+                style: TextStyle(fontSize: 25),
+              )),
+
+          SizedBox(height: 2.h,),
+          ElevatedButton(onPressed: (){
+            showDialog(context: context, builder: (context){
+              return AlertDialog(
+                title: Text("OK?"),
+                content: Text("This is an Alert Dialog"),
+                actions: [
+                  TextButton(onPressed: (){
+                    Navigator.pop(context);
+              }, child: Text("Cancle")),
+                  TextButton(onPressed: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) {
+                      return Home();
+                    }));
+                  }, child:Text("OK")),
+                ],
+              );
+            });
+          },
+              child: Text(
+                "Alert Dialog",
+                style: TextStyle(fontSize: 25),
+              )),
+
+        ],
       ),
     );
   }
